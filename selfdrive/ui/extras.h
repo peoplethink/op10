f352edacbf4cf9cdc91144cafa1bd1968e7c50b4
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-07-27 22:20:18 +0900

    Update extras.h

--- selfdrive/ui/extras.h
+++ selfdrive/ui/extras.h
@@ -15,7 +15,7 @@ static void ui_draw_extras_limit_speed(UIState *s)
     {
         int w = 185;
         int h = 60;
-        int x = (s->fb_w + (bdr_s*2))/2 - w/2 - bdr_s - 850;
+        int x = (s->fb_w + (bdr_s*2))/2 - w/2 - bdr_s - 844;
         int y = 238;
 
         const char* img = activeNDA == 1 ? "img_nda" : "img_hda";
