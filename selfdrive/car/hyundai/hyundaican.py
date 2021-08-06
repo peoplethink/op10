5069d6331e54780aa016798ff1b1aa6d6c564aff
parkbyeoungkwon <72653549+parkbyeoungkwon@users.noreply.github.com>
2021-08-07 07:51:50 +0900

    HDA
    1.NDA HDA 연동시 고속도로에서만 사용
    2.계기판에 HDA 활성화
    3.HUD (초록색) 핸들,차선활성화,속도표시

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -94,8 +94,8 @@ def create_lfahda_mfc(packer, enabled, active):
 def create_hda_mfc(packer, active):
   values = {
     "HDA_USM": 2,
-    "HDA_Active": 1 if active > 0 else 0,
-    "HDA_Icon_State": 2 if active > 0 else 0,
+    "HDA_Active": 1 if active > 1 else 0,  #cluster
+    "HDA_Icon_State": 0 if active > 1 else 0, #HUD
   }
 
   return packer.make_can_msg("LFAHDA_MFC", 0, values)
