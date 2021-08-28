3d0216d78c91c373c2ef9676bd3b749ef6ae3b04
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-08-28 13:45:24 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -85,7 +85,7 @@ class CarInterface(CarInterfaceBase):
     elif lat_control_method == 2:
       ret.lateralTuning.init('lqr')
       
-      ret.lateralTuning.lqr.scale = 1680.
+      ret.lateralTuning.lqr.scale = 1670.
       ret.lateralTuning.lqr.ki = 0.01
       ret.lateralTuning.lqr.dcGain = 0.002858
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
@@ -93,33 +93,34 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.lqr.c = [1., 0.]
       ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
       ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-      ret.steerRatio = 15.0
-
-    ret.steerActuatorDelay = 0.0
-    ret.steerLimitTimer = 2.0
-    ret.steerRateCost = 0.552
-    ret.steerMaxBP = [0.]
-    ret.steerMaxV = [1.5]
+      
+      ret.steerRatio = 16.5
+      ret.steerActuatorDelay = 0.97
+      ret.steerLimitTimer = 3.0
+      ret.steerRateCost = 0.4
+      ret.steerMaxBP = [0.]
+      ret.steerMaxV = [2.0]
     
     # longitudinal
-    ret.longitudinalTuning.kpBP = [0., 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 40.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 100.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.2, 0.97, 0.82, 0.735, 0.63, 0.54, 0.46]
-    ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kiV = [0.03, 0.02]
-    ret.longitudinalTuning.kfBP = [0.]
-    ret.longitudinalTuning.kfV = [1.0]
-    ret.longitudinalTuning.deadzoneBP = [0., 100.*CV.KPH_TO_MS]
+    
+    ret.longitudinalTuning.kpBP = [0, 10. * CV.KPH_TO_MS, 20. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 70. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
+    ret.longitudinalTuning.kpV = [0.674, 0.654, 0.65, 0.5, 0.38, 0.32, 0.20]
+    ret.longitudinalTuning.kiBP = [0., 30. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS,]
+    ret.longitudinalTuning.kiV = [0.0115, 0.015, 0.0155]
+    ret.longitudinalTuning.kfBP = [50. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS]
+    ret.longitudinalTuning.kfV = [0.79, 0.48]
+    ret.longitudinalTuning.deadzoneBP = [0., 100. * CV.KPH_TO_MS]
     ret.longitudinalTuning.deadzoneV = [0., 0.015]
+    
+    ret.gasMaxBP = [0., 5. * CV.KPH_TO_MS, 10. * CV.KPH_TO_MS,  30. * CV.KPH_TO_MS, 45. * CV.KPH_TO_MS, 70. * CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
+    ret.gasMaxV = [0.1, 0.3, 0.36, 0.31, 0.21, 0.14, 0.11]
 
-    ret.gasMaxBP = [0., 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 50.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.gasMaxV = [0.628, 0.448, 0.355, 0.24, 0.17, 0.13]
-
-    ret.brakeMaxBP = [0, 70.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.brakeMaxV = [1.5, 1.0, 0.7]
-
+    ret.brakeMaxBP = [0, 5. * CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 50*CV.KPH_TO_MS, 65*CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
+    ret.brakeMaxV = [0.3, 0.67, 0.8556, 0.845, 0.835, 0.83]
     ret.stoppingBrakeRate = 0.15  # brake_travel/s while trying to stop
-    ret.startingBrakeRate = 1.0  # brake_travel/s while releasing on restart
-    ret.startAccel = 1.55
+    ret.startingBrakeRate = 0.99  # brake_travel/s while releasing on restart
+    ret.startAccel = 1.46
+
 
     # genesis
     if candidate == CAR.GENESIS:
