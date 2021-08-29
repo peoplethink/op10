08a66bc239c0af0e1485fa516bc7b0273f4dccad
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-08-29 19:58:16 +0900

    Update latcontrol_pid.py

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -9,9 +9,9 @@ class LatControlPID():
   def __init__(self, CP):
     self.pid = PIController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
                             (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
-                            ([0.], [CP.lateralTuning.pid.kf]),
-                            pos_limit=1.0, neg_limit=-1.0, sat_limit=CP.steerLimitTimer)
-
+                            (CP.lateralTuning.pid.kdBP, CP.lateralTuning.pid.kdV),
+                            k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, neg_limit=-1.0,
+                            sat_limit=CP.steerLimitTimer)
   def reset(self):
     self.pid.reset()
 
