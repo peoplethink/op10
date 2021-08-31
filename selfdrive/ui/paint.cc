a75e80eda2ed52144d03a90c362a30af50d448a4
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-08-31 22:11:28 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1154,7 +1154,7 @@ static void draw_laneless_button(UIState *s) {
     int btn_w = 153;
     int btn_h = 153;
     int btn_x1 = s->fb_w - btn_w - 195 -20 - 20 + 195;
-    int btn_y = 1080 - btn_h - 35 - (btn_h / 2) + 39 - 120;
+    int btn_y = 1080 - btn_h - 35 - (btn_h / 2) + 39 - 150;
     int btn_xc1 = btn_x1 + (btn_w/2);
     int btn_yc = btn_y + (btn_h/2);
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
@@ -1255,7 +1255,7 @@ static void ui_draw_tpms(UIState *s) {
   int viz_tpms_w = 140;
   int viz_tpms_h = 130;
   int viz_tpms_x = s->fb_w - 185;
-  int viz_tpms_y = 690 - 105;
+  int viz_tpms_y = 690 - 125;
   
   char tpmsFl[32];
   char tpmsFr[32];
@@ -1266,7 +1266,7 @@ static void ui_draw_tpms(UIState *s) {
   ui_draw_rect(s->vg, rect, COLOR_WHITE_ALPHA(80), 5, 20);
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
   const int pos_x = viz_tpms_x + (viz_tpms_w / 2);
-  const int pos_y = 796 - 105;
+  const int pos_y = 796 - 125;
   const int pos_add = 38;
   const int fontsize = 65;
 
