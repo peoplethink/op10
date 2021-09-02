a4f51c5d6350b131f45f6e58d1f77ccf000fe826
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-09-02 10:52:37 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -118,7 +118,7 @@ class CarInterface(CarInterfaceBase):
     
     ret.stoppingBrakeRate = 0.15  # brake_travel/s while trying to stop
     ret.startingBrakeRate = 1.0  # brake_travel/s while releasing on restart
-    ret.startAccel = 1.5
+    ret.startAccel = 1.6
     
     ret.steerMaxBP = [0.]
     ret.steerMaxV = [1.5]
