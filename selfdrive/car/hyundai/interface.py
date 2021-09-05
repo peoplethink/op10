2b2c6264390f9e4a62048c39bebd4e5f419301a6
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-09-05 10:44:24 +0900

    Revert "smare mdps"
    This reverts commit af866e6beb315018e8cb2d7398f03f7f407aec55.

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
-import os
-import shutil
-from os import path
+
 from cereal import car
 from selfdrive.config import Conversions as CV
 from selfdrive.car.hyundai.values import Ecu, ECU_FINGERPRINT, CAR, FINGERPRINTS, Buttons, FEATURES
