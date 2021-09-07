2226e114544b368aecd0d5795aba7ec64f4ed1b4
marine2875 <79120100+marine2875@users.noreply.github.com>
2021-09-07 21:38:25 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -26,7 +26,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
 
     gas_max_bp = [0., 10., 20., 50., 70., 130.]
-    gas_max_v = [1.9, 1.25, 0.87, 0.63, 0.44, 0.33]
+    gas_max_v = [1.9, 1.35, 0.97, 0.63, 0.44, 0.33]
 
     brake_max_bp = [0, 70., 130.]
     brake_max_v = [-4., -3., -2.1]
@@ -69,13 +69,13 @@ class CarInterface(CarInterfaceBase):
       #ret.lateralTuning.pid.kdBP = [0.]
       #ret.lateralTuning.pid.kdV = [1.0]
       
-      ret.lateralTuning.pid.kf = 0.00003
+      ret.lateralTuning.pid.kf = 0.00004
       ret.lateralTuning.pid.kpBP = [11., 25.]
-      ret.lateralTuning.pid.kpV = [0.10, 0.125]
+      ret.lateralTuning.pid.kpV = [0.10, 0.12]
       ret.lateralTuning.pid.kiBP = [11., 25.]
       ret.lateralTuning.pid.kiV = [0.002, 0.02]
       ret.lateralTuning.pid.kdBP = [11., 25.]
-      ret.lateralTuning.pid.kdV = [0.5, 0.5]
+      ret.lateralTuning.pid.kdV = [0.1, 0.25]
       ret.steerActuatorDelay = 0.1
       ret.steerRateCost = 0.4
       ret.steerLimitTimer = 2.0
@@ -121,7 +121,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kpBP = [0., 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 40.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 100.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
     ret.longitudinalTuning.kpV = [1.3, 0.98, 0.83, 0.75, 0.655, 0.57, 0.48]
     ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kiV = [0.05, 0.03]
+    ret.longitudinalTuning.kiV = [0.04, 0.03]
     ret.longitudinalTuning.kdBP = [0., 4., 9., 17., 23., 31.]
     ret.longitudinalTuning.kdV = [0.7, 0.65, 0.5, 0.4, 0.3, 0.2]
     ret.longitudinalTuning.deadzoneBP = [0., 100.*CV.KPH_TO_MS]
